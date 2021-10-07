93bedb57ac5d6f2a21564c564ba69a892097c310
Cameron H <cameronheadlee6@gmail.com>
2021-10-06 20:01:45 -0400

    made changes to syntax in the workoutSchema

--- models/workout.js
+++ models/workout.js
@@ -3,21 +3,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date(),
+    },
     exercises: [
-    {
-    type: String,
-    name: String,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets:Number,
-    distance: Number
-   }],
-   day: {
-    type: Date,
-    default: Date.now
-   }
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: 'Enter an exercise type',
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: 'Enter and exercise name',
+            },
+            duration: {
+                type: Number,
+                required: 'Enter an exercise duration in minutes',
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+        },
+    ],
 });
+    
 
 const workout = mongoose.model("Workout", workoutSchema);
 
