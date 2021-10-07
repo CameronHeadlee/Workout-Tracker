582156b506685ed71b1c579ec73558190a5666b7
Cameron H <cameronheadlee6@gmail.com>
2021-10-07 15:19:31 -0400

    Got the continue workout button to render with changes to front end as well as the stats to render on the view page

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
 
-app.use(express.static("public"));
+mongoose.connect('mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, });
-
-app.use(require("./routes/api.js"));
-app.use(require("./routes/home.js"));
+// routes
+app.use(require('./routes/api.js'));
+app.use(require('./routes/view.js'));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
   
